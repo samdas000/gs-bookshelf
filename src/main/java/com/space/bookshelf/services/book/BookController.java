c8cf3658db77c88bbbbde69b8610644089e8e882
samdas000 <samit.das@gmail.com>
2019-06-14 13:59:32 +0530

    Update BookController.java

--- src/main/java/com/space/bookshelf/services/book/BookController.java
+++ src/main/java/com/space/bookshelf/services/book/BookController.java
@@ -1,8 +1,12 @@
 package com.space.bookshelf.services.book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.space.bookshelf.services.book.dao.BookDAO;
@@ -11,7 +15,6 @@ import com.space.bookshelf.services.donor.dao.DonorDAO;
 import com.space.bookshelf.services.donor.model.Donor;
 import com.space.bookshelf.system.BeanUtil;
 
-
 @RestController
 public class BookController 
 {
@@ -19,8 +22,8 @@ public class BookController
 	private BookDAO dao;
 	
 	
-	@RequestMapping("/punya/book/add")
-	public String getListofBooks()
+	@RequestMapping("/punya/book/add_old")
+	public String getListofBooks_old()
 	{
 		Book book = new Book();
 		
@@ -39,13 +42,74 @@ public class BookController
 	}
 	
 	
-	@RequestMapping("punya/book/show")
-	public String Show()
+	@RequestMapping("/punya/book/add")
+	public String getListofBooks(@RequestParam(name="stream") String stream,
+			@RequestParam(name="standard") String standard,
+			@RequestParam(name="subject") String subject,
+			@RequestParam(name="author") String author,
+			@RequestParam(name="title") String title,
+			@RequestParam(name="edition") String edition)
 	{
-		//return dao.getAllBooks().toString();	
-		return BeanUtil.getJSon(dao.getAllBooks());
+		Book book = new Book();
+		
+		String message = "";
+		
+		if((stream != null) && (standard != null) && (subject != null) && 
+				(author != null) && (title != null) && (edition != null))
+		{
+			//book.setId("1");
+	        book.setStream(stream);
+	        book.setStandard(standard);
+	        book.setSubject(subject);
+	        book.setAuthor(author);
+	        book.setTitle(title);
+	        book.setEdition(edition);
+	        
+	        int i = dao.addBook(book);
+	        if(i==1)
+	        {
+	        	message = "Book details added successfully";
+	        }
+			
+		}
+		else
+		{
+			message = "Mandatory fields: stream, standard, subject, author, title, edition"+
+					  "USE:\"stream\"=\"<stream>\",\"standard\"=\"<standard>\",\"subject\"=\"<subject>\","
+					  + "\"author\"=\"<author>\",\"title\"=\"<title>\",\"title\"=\"<title>\"";
+		}
+		return BeanUtil.getJSon(message);
+		
+		//return "index"; later need to forward to boot catalogue page
+	}
+	
+	
+	
+		
+	@RequestMapping("punya/book/show")
+	public String show()
+	{
+		Book book = new Book();
+		
+		book.setId("1");
+        book.setStream("ICSE");
+        book.setStandard("Class-IX");
+        book.setSubject("History");
+        book.setAuthor("Dr Ramesh");
+        book.setTitle("History of Independent India");
+        book.setEdition("2.0");
+        
+        List test = new ArrayList();
+        test.add (book);        
+        test.add (book);        
+        test.add (book);        
+        //return BeanUtil.getJSon(test);
+        return BeanUtil.getJSon(dao.getAllBooks());
+        
+		//return dao.getAllBooks().toString();		
 		
 	}
+	
 	
 	@RequestMapping("punya/book/{id}")
 	public String showDetails(@PathVariable("id") int id)
@@ -53,7 +117,6 @@ public class BookController
 		Book book =  dao.getBook(id);
 		return BeanUtil.getJSon(book);    
 	}
-	
 	
 	
 
